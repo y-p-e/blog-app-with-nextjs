5c232490228b2f18ba5fce79dd7368110204f627
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2022-11-30 21:21:05 +0900

    ヘッダーコンポーネント作成

---
+++ components/organisms/Header/index.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import Header from './index'
+
+export default {
+	title: 'organisms/Header',
+	argTypes: {
+		title: {
+			control: {type: 'text'},
+			description: 'タイトル',
+			table: {
+				type: {summary: 'string'},
+			}
+		},
+	}
+} as ComponentMeta<typeof Header>
+
+const Template: ComponentStory<typeof Header> = (args) => (
+	<Header {...args}/>
+)
+
+// 管理画面用ヘッダー
+export const BackOffice = Template.bind({})
+BackOffice.args = {
+	title: '管理画面',
+}
---
+++ components/organisms/Header/index.tsx
@@ -0,0 +1,56 @@
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import LogoutIcon from '@mui/icons-material/Logout';
+import Grid from '@mui/material/Grid';
+
+type HeaderProps = {
+	title: string,
+}
+
+const Header = (props: HeaderProps) => {
+	const {title} = props
+	return (
+    <Box sx={{ flexGrow: 1 }}>
+		<Grid container spacing ={1} direction="row" justifyContent="space-between">
+      <AppBar position="static">
+        <Toolbar>
+						<Grid item xs={1}>
+							<IconButton
+								size="large"
+								edge="start"
+								color="inherit"
+								aria-label="menu"
+								sx={{ mr: 2 }}
+							>
+								<MenuIcon />
+							</IconButton>
+						</Grid>
+						<Grid item xs={10}>
+							<Typography variant="h6" component="div" align='center'>
+								{title}
+							</Typography>
+						</Grid>
+						<Grid item xs={1} sx={{textAlign: "right"}}>
+							<IconButton
+								size="large"
+								edge="start"
+								color="inherit"
+								aria-label="menu"
+								sx={{ mr: 2}}
+							>
+								<LogoutIcon />
+							</IconButton>
+						</Grid>
+        </Toolbar>
+      </AppBar>
+		</Grid>
+    </Box>
+	)
+}
+
+export default Header
