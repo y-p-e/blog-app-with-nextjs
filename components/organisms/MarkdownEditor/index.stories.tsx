0db76da6039a0732e422c65398a20738463ba482
Yuki Tanio <yuki_tanio@yahoo.co.jp>
2022-12-12 00:01:14 +0900

    MarkdownEditorストーリーブック修正

--- components/organisms/MarkdownEditor/index.stories.tsx
+++ components/organisms/MarkdownEditor/index.stories.tsx
@@ -3,8 +3,31 @@ import MarkdownEditor from './index'
 
 export default {
   title: 'organisms/MarkdownEditor',
+  argTypes: {
+    text: {
+      control: { type: 'string' },
+      description: 'マークダウン',
+      table: {
+        type: { summary: 'string' },
+      },
+    },
+    setText: {
+      description: 'チェンジイベントハンドラ',
+      table: {
+        type: { summary: 'number' },
+      },
+    },
+  },
 } as ComponentMeta<typeof MarkdownEditor>
 
-const Template: ComponentStory<typeof MarkdownEditor> = () => <MarkdownEditor />
+const Template: ComponentStory<typeof MarkdownEditor> = (args) => (
+  <MarkdownEditor {...args} />
+)
 
 export const Editor = Template.bind({})
+Editor.args = {
+  text: 'test',
+  setText: () => {
+    // do nothing.
+  },
+}
